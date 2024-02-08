dd44d74ed9fa0b9f6e84859c8e43dc787592101c
aksylumoed <adityadndkr@gmail.com>
2024-02-08 20:21:21 +0100

    Fix artwork descriptions

--- constants.ts
+++ constants.ts
@@ -12,7 +12,7 @@ export const artworks: Artwork[] = [
     title: "#8",
     imagePath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/png/8.png",
     dziPath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/8.dzi",
-    description: `190cm x 97.5cm\naluminium dibond, direct print`,
+    description: `190cm x 190cm\naluminium dibond, direct print`,
     maxWidthPercentage: "45%",
     maxWidthPercentageMobile: "75%"
   },
@@ -20,7 +20,7 @@ export const artworks: Artwork[] = [
       title: "#5",
       imagePath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/png/5.png",
       dziPath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/5.dzi",
-      description: `190cm x 97.5cm\naluminium dibond, direct print`,
+      description: `95.3cm x 190cm aluminium dibond, direct print`,
       maxWidthPercentage: "25%",
       maxWidthPercentageMobile: "50%"
   },
@@ -28,7 +28,7 @@ export const artworks: Artwork[] = [
     title: "#4",
     imagePath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/png/4.png",
     dziPath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/4.dzi",
-    description: `190cm x 97.5cm\naluminium dibond, direct print`,
+    description: `300cm x 90cm aluminium dibond, direct print`,
     maxWidthPercentage: "70%",
     maxWidthPercentageMobile: "80%"
   },
@@ -36,7 +36,7 @@ export const artworks: Artwork[] = [
     title: "#2",
     imagePath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/png/2.png",
     dziPath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/2.dzi",
-    description: `190cm x 97.5cm\naluminium dibond, direct print`,
+    description: `160cm x 90cm aluminium dibond, direct print`,
     maxWidthPercentage: "60%",
     maxWidthPercentageMobile: "80%"
   },
@@ -44,7 +44,7 @@ export const artworks: Artwork[] = [
     title: "#3",
     imagePath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/png/3.png",
     dziPath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/3.dzi",
-    description: `190cm x 97.5cm\naluminium dibond, direct print`,
+    description: `135cm x 90cm aluminium dibond, direct print`,
     maxWidthPercentage: "60%",
     maxWidthPercentageMobile: "80%"
   },
@@ -52,7 +52,7 @@ export const artworks: Artwork[] = [
     title: "#6",
     imagePath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/png/6.png",
     dziPath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/6.dzi",
-    description: `190cm x 97.5cm\naluminium dibond, direct print`,
+    description: `126.7cm x 190cm aluminium dibond, direct print`,
     maxWidthPercentage: "30%",
     maxWidthPercentageMobile: "55%"
   },
@@ -60,7 +60,7 @@ export const artworks: Artwork[] = [
     title: "#1",
     imagePath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/png/1.png",
     dziPath: "https://adndkr-art.s3.eu-north-1.amazonaws.com/1.dzi",
-    description: `190cm x 97.5cm\naluminium dibond, direct print`,
+    description: `150cm x 100cm aluminium dibond, direct print`,
     maxWidthPercentage: "60%",
     maxWidthPercentageMobile: "75%"
   },
